417767131a161a0efeeeb32d6b18aad5683b0e6a
Roman Voloboev <roman.voloboev@privatbank.ua>
2017-10-06 16:04:00 +0300

    ...

--- src/main/java/com/romanvoloboev/controller/MainViewController.java
+++ src/main/java/com/romanvoloboev/controller/MainViewController.java
@@ -2,11 +2,16 @@ package com.romanvoloboev.controller;
 
 import com.romanvoloboev.service.MainViewService;
 import de.felixroske.jfxsupport.FXMLController;
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sound.sampled.LineUnavailableException;
+import java.util.concurrent.Callable;
 
 /**
  * Created at 05.10.17
@@ -15,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @FXMLController
 public class MainViewController {
-
+    private static final Logger log = LoggerFactory.getLogger(MainViewController.class);
     private final MainViewService mainViewService;
 
     @Autowired
@@ -41,8 +46,32 @@ public class MainViewController {
     @FXML
     private Button exitBtn;
 
-    public void doStartAction(Event event) {
-        String text = mainViewService.getSomeText();
-        textArea.setText(text);
+    public void doStartRecord(ActionEvent event) {
+
+        log.info("start!!!");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainViewService.startRecord(textArea);
+                } catch (LineUnavailableException | InterruptedException e) {
+                    log.error("error: ", e);
+                }
+            }
+        };
+
+        Thread t = new Thread(runnable);
+        t.setDaemon(true);
+        t.start();
+
+        startBtn.setDisable(true);
+        stopBtn.setDisable(false);
+    }
+
+    public void doStopRecord(ActionEvent actionEvent) {
+        log.info("stop!!");
+        mainViewService.stopRecord();
+        startBtn.setDisable(false);
+        stopBtn.setDisable(true);
     }
 }
--- src/main/java/com/romanvoloboev/service/MainViewService.java
+++ src/main/java/com/romanvoloboev/service/MainViewService.java
@@ -1,6 +1,18 @@
 package com.romanvoloboev.service;
 
+import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
+import com.darkprograms.speech.recognizer.GSpeechResponseListener;
+import com.darkprograms.speech.recognizer.GoogleResponse;
+import javafx.scene.control.TextArea;
+import net.sourceforge.javaflacencoder.FLACFileWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import javax.sound.sampled.LineUnavailableException;
 
 /**
  * Created at 05.10.17
@@ -8,9 +20,77 @@ import org.springframework.stereotype.Service;
  * @author romanvoloboev
  */
 @Service
-public class MainViewService {
+public class MainViewService implements GSpeechResponseListener {
+    private static final Logger log = LoggerFactory.getLogger(MainViewService.class);
+    private final Microphone microphone;
+    private final GSpeechDuplex duplex;
+    private final Environment env;
+    private final String API_KEY;
+
+    @Autowired
+    public MainViewService(Environment env) {
+        this.env = env;
+        API_KEY = env.getProperty("google_api_key");
+        log.info("API KEY: {}", API_KEY);
+
+        microphone = new Microphone(FLACFileWriter.FLAC);
+        duplex = new GSpeechDuplex(API_KEY);
+        duplex.setLanguage("ru");
+    }
 
     public String getSomeText() {
         return "HELLOO111";
+    }
+
+    public void startRecord(TextArea textArea) throws LineUnavailableException, InterruptedException {
+        log.info("--- Starting Speech Recognition, Microphone State is: {}",  microphone.getState());
+        log.info("--- AudioFormat: {}", microphone.getAudioFormat());
+
+        duplex.recognize(microphone.getTargetDataLine(), microphone.getAudioFormat());
+
+        duplex.addResponseListener(new GSpeechResponseListener() {
+            String old_text = "";
+
+            public void onResponse(GoogleResponse gr) {
+                log.info("--- onResponse");
+                String output = "";
+                output = gr.getResponse();
+                log.info("--- resp: "+gr.getResponse());
+                if (gr.getResponse() == null) {
+                    this.old_text = textArea.getText();
+                    if (this.old_text.contains("(")) {
+                        this.old_text = this.old_text.substring(0, this.old_text.indexOf('('));
+                    }
+                    log.info("Paragraph Line Added");
+                    this.old_text = ( textArea.getText() + "\n" );
+                    this.old_text = this.old_text.replace(")", "").replace("( ", "");
+                    textArea.setText(this.old_text);
+                    return;
+                }
+                if (output.contains("(")) {
+                    output = output.substring(0, output.indexOf('('));
+                }
+                if (!gr.getOtherPossibleResponses().isEmpty()) {
+                    output = output + " (" + (String) gr.getOtherPossibleResponses().get(0) + ")";
+                }
+                log.info(output);
+                textArea.setText("");
+                textArea.appendText(this.old_text);
+                textArea.appendText(output);
+            }
+        });
+
+    }
+
+
+    public void stopRecord() {
+        microphone.close();
+        duplex.stopSpeechRecognition();
+        log.info("Stopping Speech Recognition, Microphone State is: {}",  microphone.getState());
+    }
+
+    @Override
+    public void onResponse(GoogleResponse googleResponse) {
+
     }
 }
