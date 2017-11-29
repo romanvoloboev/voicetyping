a6519f40bed25638317a0afe510c2c4ab30f2169
Roman Voloboev <roman.voloboev@privatbank.ua>
2017-11-29 12:29:10 +0200

    ...

--- src/main/java/com/romanvoloboev/controller/MainViewController.java
+++ src/main/java/com/romanvoloboev/controller/MainViewController.java
@@ -1,6 +1,13 @@
 package com.romanvoloboev.controller;
 
-import com.romanvoloboev.service.MainViewService;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.rpc.ApiStreamObserver;
+import com.google.api.gax.rpc.BidiStreamingCallable;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.speech.v1.*;
+import com.google.protobuf.ByteString;
+import com.romanvoloboev.utils.v1.Microphone;
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -9,12 +16,18 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created at 05.10.17
@@ -22,18 +35,26 @@ import java.io.IOException;
  * @author romanvoloboev
  */
 @FXMLController
-public class MainViewController {
+public class MainViewController  {
     private static final Logger log = LoggerFactory.getLogger(MainViewController.class);
-    private final MainViewService mainViewService;
+    private static Microphone microphone;
+    private static int buffSize;
+    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private static ApiStreamObserver<StreamingRecognizeRequest> requestObserver;
+    private static ResponseApiStreamingObserver responseObserver;
+
+
 
     @FXML
     private TextArea textArea;
 
     @Autowired
-    public MainViewController(MainViewService mainViewService) {
-        this.mainViewService = mainViewService;
-        mainViewService.setTextArea(textArea);
+    public MainViewController(Microphone mic) {
+        microphone = mic;
+        buffSize = microphone.getTargetDataLine().getBufferSize();
+        startRecognition();
     }
+
 
     public void openAction(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -43,7 +64,7 @@ public class MainViewController {
         File file = fileChooser.showOpenDialog(new Stage());
         if (file != null) {
             try {
-                mainViewService.getText(file);
+                getText(file);
             } catch (IOException e) {
                 new Alert(Alert.AlertType.ERROR, "Неподдерживаемый формат файла.").showAndWait();
             }
@@ -63,7 +84,7 @@ public class MainViewController {
         File fileWhereSave = fileChooser.showSaveDialog(new Stage());
         if (fileWhereSave != null) {
             try {
-                mainViewService.saveTextAreaToFile(fileWhereSave);
+                saveTextAreaToFile(fileWhereSave);
             } catch (IOException e) {
                 new Alert(Alert.AlertType.ERROR, "Не удалось сохранить файл").showAndWait();
             }
@@ -73,32 +94,124 @@ public class MainViewController {
 
 
 
-//    public void doStartRecord(ActionEvent event) {
-//
-//        log.info("start!!!");
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    mainViewService.startRecord(textArea);
-//                } catch (LineUnavailableException | InterruptedException e) {
-//                    log.error("error: ", e);
-//                }
-//            }
-//        };
-//
-//        Thread t = new Thread(runnable);
-//        t.setDaemon(true);
-//        t.start();
-//
-//        startBtn.setDisable(true);
-//        stopBtn.setDisable(false);
-//    }
-//
-//    public void doStopRecord(ActionEvent actionEvent) {
-//        log.info("stop!!");
-//        mainViewService.stopRecord();
-//        startBtn.setDisable(false);
-//        stopBtn.setDisable(true);
-//    }
+
+    private void getText(File file) throws IOException {
+        String contentType = Files.probeContentType(file.toPath());
+        if (contentType == null || !contentType.equals("text/plain")
+                && !contentType.equals("application/msword")
+                && !contentType.equals("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+            throw new IOException();
+        } else if (contentType.equals("application/msword") || contentType.equals("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+            XWPFDocument document = new XWPFDocument(new FileInputStream(file));
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+            textArea.setText(extractor.getText());
+        } else {
+            textArea.setWrapText(true);
+            textArea.clear();
+            Files.lines(file.toPath()).forEachOrdered(s -> textArea.appendText(s+"\n"));
+        }
+    }
+
+
+    private void saveTextAreaToFile(File fileWhereSave) throws IOException {
+        try (BufferedWriter bf = new BufferedWriter(new FileWriter(fileWhereSave))) {
+            bf.write(textArea.getText());
+        }
+    }
+
+    private static Credentials loadCredentialsFromFile() throws IOException {
+        String credentialsFile = "classpath:credentials.json";
+        log.info("Loading credentials from specified file: {}", credentialsFile);
+        try (FileInputStream fileInputStream = new FileInputStream(ResourceUtils.getFile(credentialsFile))) {
+            Credentials credentials = ServiceAccountCredentials.fromStream(fileInputStream);
+            log.info("Successfully loaded from file.");
+            return credentials;
+        }
+    }
+
+    private static class RecognitionTask implements Callable<Void> {
+        @Override
+        public Void call() throws Exception {
+            byte data[] = new byte[buffSize];
+            initRecognition();
+            while (microphone.getState() == Microphone.State.BUSY) {
+                int bytesRead = microphone.getTargetDataLine().read(data, 0, buffSize);
+                if (bytesRead > 0) {
+                    recognizeData(data, bytesRead);
+                } else {
+                    log.error("0 bytes readed");
+                }
+            }
+            return null;
+        }
+    }
+
+
+    public void stopRecognition() {
+        requestObserver.onCompleted();
+        microphone.stopRecording();
+    }
+
+    public void startRecognition() {
+        microphone.startRecording();
+        executorService.submit(new RecognitionTask());
+    }
+
+    private static void recognizeData(byte[] data, int size) {
+        log.info("sending recognition request");
+        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setAudioContent(ByteString.copyFrom(data, 0, size)).build());
+    }
+
+
+    private static void initRecognition() throws IOException {
+
+        SpeechSettings speechSettings = SpeechSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(loadCredentialsFromFile()))
+                .build();
+        SpeechClient speech = SpeechClient.create(speechSettings);
+
+
+        RecognitionConfig recognitionConfig = RecognitionConfig.newBuilder()
+                .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                .setLanguageCode("ru-RU")
+                .setSampleRateHertz(16000)
+                .build();
+
+        StreamingRecognitionConfig streamingRecognitionConfig = StreamingRecognitionConfig.newBuilder()
+                .setConfig(recognitionConfig).setInterimResults(false)
+                .build();
+
+        responseObserver = new ResponseApiStreamingObserver();
+        BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable = speech.streamingRecognizeCallable();
+        requestObserver = callable.bidiStreamingCall(responseObserver);
+
+
+        //init request
+        log.info("sending INIT recognition request");
+        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setStreamingConfig(streamingRecognitionConfig).build());
+    }
+
+    static class ResponseApiStreamingObserver implements ApiStreamObserver<StreamingRecognizeResponse> {
+        @Override
+        public void onNext(StreamingRecognizeResponse message) {
+            if (message.getError().getCode() == 11) {
+                requestObserver.onCompleted();
+                executorService.submit(new RecognitionTask());
+            }
+
+            log.info("==== raw resp: {}", message.toString());
+            log.info("==== msg: {}", message.getResultsList().get(0).getAlternatives(0).getTranscript());
+        }
+
+        @Override
+        public void onError(Throwable t) {
+        }
+
+        @Override
+        public void onCompleted() {
+            if (responseObserver != null) responseObserver = null;
+            if (requestObserver != null) requestObserver = null;
+        }
+
+    }
 }
--- src/main/java/com/romanvoloboev/utils/v1/GoogleSpeechRecognizeService.java
+++ src/main/java/com/romanvoloboev/utils/v1/GoogleSpeechRecognizeService.java
@@ -25,111 +25,112 @@ import java.util.concurrent.*;
 @Service
 public class GoogleSpeechRecognizeService {
     private static final Logger log = LoggerFactory.getLogger(GoogleSpeechRecognizeService.class);
-    private static Microphone microphone;
-    private static int buffSize;
-    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
-    private static ApiStreamObserver<StreamingRecognizeRequest> requestObserver;
-    private static ResponseApiStreamingObserver responseObserver;
+//    private static Microphone microphone;
+//    private static int buffSize;
+//    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+//    private static ApiStreamObserver<StreamingRecognizeRequest> requestObserver;
+//    private static ResponseApiStreamingObserver responseObserver;
 
 
-    private static Credentials loadCredentialsFromFile() throws IOException {
-        String credentialsFile = "classpath:credentials.json";
-        log.info("Loading credentials from specified file: {}", credentialsFile);
-        try (FileInputStream fileInputStream = new FileInputStream(ResourceUtils.getFile(credentialsFile))) {
-            Credentials credentials = ServiceAccountCredentials.fromStream(fileInputStream);
-            log.info("Successfully loaded from file.");
-            return credentials;
-        }
-    }
+//    private static Credentials loadCredentialsFromFile() throws IOException {
+//        String credentialsFile = "classpath:credentials.json";
+//        log.info("Loading credentials from specified file: {}", credentialsFile);
+//        try (FileInputStream fileInputStream = new FileInputStream(ResourceUtils.getFile(credentialsFile))) {
+//            Credentials credentials = ServiceAccountCredentials.fromStream(fileInputStream);
+//            log.info("Successfully loaded from file.");
+//            return credentials;
+//        }
+//    }
 
-    public GoogleSpeechRecognizeService(Microphone mic) throws InterruptedException, ExecutionException, TimeoutException {
-        microphone = mic;
-        buffSize = microphone.getTargetDataLine().getBufferSize();
-        microphone.startRecording();
+//    public GoogleSpeechRecognizeService(Microphone mic) throws InterruptedException, ExecutionException, TimeoutException {
+//        microphone = mic;
+//        buffSize = microphone.getTargetDataLine().getBufferSize();
+//    }
 
-        executorService.submit(new RecognitionTask());
-
-    }
-
-    private static class RecognitionTask implements Callable<Void> {
-        @Override
-        public Void call() throws Exception {
-            byte data[] = new byte[buffSize];
-            initRecognition();
-            while (microphone.getState() == Microphone.State.BUSY) {
-                int bytesRead = microphone.getTargetDataLine().read(data, 0, buffSize);
-                if (bytesRead > 0) {
-                    recognizeData(data, bytesRead);
-                } else {
-                    log.error("0 bytes readed");
-                }
-            }
-            return null;
-        }
-    }
-
-
-    public void stopRecognition() {
-        requestObserver.onCompleted();
-        microphone.stopRecording();
-    }
-
-    private static void recognizeData(byte[] data, int size) {
-        log.info("sending recognition request");
-        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setAudioContent(ByteString.copyFrom(data, 0, size)).build());
-    }
-
-
-    private static void initRecognition() throws IOException {
-
-        SpeechSettings speechSettings = SpeechSettings.newBuilder()
-                .setCredentialsProvider(FixedCredentialsProvider.create(loadCredentialsFromFile()))
-                .build();
-        SpeechClient speech = SpeechClient.create(speechSettings);
-
-
-        RecognitionConfig recognitionConfig = RecognitionConfig.newBuilder()
-                .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
-                .setLanguageCode("ru-RU")
-                .setSampleRateHertz(16000)
-                .build();
-
-        StreamingRecognitionConfig streamingRecognitionConfig = StreamingRecognitionConfig.newBuilder()
-                .setConfig(recognitionConfig).setInterimResults(false)
-                .build();
-
-        responseObserver = new ResponseApiStreamingObserver();
-        BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable = speech.streamingRecognizeCallable();
-        requestObserver = callable.bidiStreamingCall(responseObserver);
-
-
-        //init request
-        log.info("sending INIT recognition request");
-        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setStreamingConfig(streamingRecognitionConfig).build());
-    }
-
-    static class ResponseApiStreamingObserver implements ApiStreamObserver<StreamingRecognizeResponse> {
-        @Override
-        public void onNext(StreamingRecognizeResponse message) {
-            if (message.getError().getCode() == 11) {
-                requestObserver.onCompleted();
-                executorService.submit(new RecognitionTask());
-            }
-
-            log.info("==== raw resp: {}", message.toString());
-            log.info("==== msg: {}", message.getResultsList().get(0).getAlternatives(0).getTranscript());
-        }
-
-        @Override
-        public void onError(Throwable t) {
-        }
-
-        @Override
-        public void onCompleted() {
-            if (responseObserver != null) responseObserver = null;
-            if (requestObserver != null) requestObserver = null;
-        }
-
-    }
+//    private static class RecognitionTask implements Callable<Void> {
+//        @Override
+//        public Void call() throws Exception {
+//            byte data[] = new byte[buffSize];
+//            initRecognition();
+//            while (microphone.getState() == Microphone.State.BUSY) {
+//                int bytesRead = microphone.getTargetDataLine().read(data, 0, buffSize);
+//                if (bytesRead > 0) {
+//                    recognizeData(data, bytesRead);
+//                } else {
+//                    log.error("0 bytes readed");
+//                }
+//            }
+//            return null;
+//        }
+//    }
+//
+//
+//    public void stopRecognition() {
+//        requestObserver.onCompleted();
+//        microphone.stopRecording();
+//    }
+//
+//    public void startRecognition() {
+//        microphone.startRecording();
+//        executorService.submit(new RecognitionTask());
+//    }
+//
+//    private static void recognizeData(byte[] data, int size) {
+//        log.info("sending recognition request");
+//        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setAudioContent(ByteString.copyFrom(data, 0, size)).build());
+//    }
+//
+//
+//    private static void initRecognition() throws IOException {
+//
+//        SpeechSettings speechSettings = SpeechSettings.newBuilder()
+//                .setCredentialsProvider(FixedCredentialsProvider.create(loadCredentialsFromFile()))
+//                .build();
+//        SpeechClient speech = SpeechClient.create(speechSettings);
+//
+//
+//        RecognitionConfig recognitionConfig = RecognitionConfig.newBuilder()
+//                .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+//                .setLanguageCode("ru-RU")
+//                .setSampleRateHertz(16000)
+//                .build();
+//
+//        StreamingRecognitionConfig streamingRecognitionConfig = StreamingRecognitionConfig.newBuilder()
+//                .setConfig(recognitionConfig).setInterimResults(false)
+//                .build();
+//
+//        responseObserver = new ResponseApiStreamingObserver();
+//        BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable = speech.streamingRecognizeCallable();
+//        requestObserver = callable.bidiStreamingCall(responseObserver);
+//
+//
+//        //init request
+//        log.info("sending INIT recognition request");
+//        requestObserver.onNext(StreamingRecognizeRequest.newBuilder().setStreamingConfig(streamingRecognitionConfig).build());
+//    }
+//
+//    static class ResponseApiStreamingObserver implements ApiStreamObserver<StreamingRecognizeResponse> {
+//        @Override
+//        public void onNext(StreamingRecognizeResponse message) {
+//            if (message.getError().getCode() == 11) {
+//                requestObserver.onCompleted();
+//                executorService.submit(new RecognitionTask());
+//            }
+//
+//            log.info("==== raw resp: {}", message.toString());
+//            log.info("==== msg: {}", message.getResultsList().get(0).getAlternatives(0).getTranscript());
+//        }
+//
+//        @Override
+//        public void onError(Throwable t) {
+//        }
+//
+//        @Override
+//        public void onCompleted() {
+//            if (responseObserver != null) responseObserver = null;
+//            if (requestObserver != null) requestObserver = null;
+//        }
+//
+//    }
 
 }
